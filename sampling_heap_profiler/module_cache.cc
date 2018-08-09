39017081f62e6f666868310b292d1a46cf03b839
Findit <findit-for-me@appspot.gserviceaccount.com>
2018-08-09 16:05:28 +0000

    Revert "[sampling heap profiler] Implement CreateModuleForAddress for Win platform."
    This reverts commit abf1475f425aa7cb6c00aa3a8d953dee03c1f9f9.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision abf1475f425aa7cb6c00aa3a8d953dee03c1f9f9 as the
    culprit for failures in the build cycles as shown on:
    https://findit-for-me.appspot.com/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtL2FiZjE0NzVmNDI1YWE3Y2I2YzAwYWEzYThkOTUzZGVlMDNjMWY5ZjkM
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium.linux/Cast%20Audio%20Linux/17825
    
    Sample Failed Step: compile
    
    Original change's description:
    > [sampling heap profiler] Implement CreateModuleForAddress for Win platform.
    > 
    > Also move the function and its helpers to ModuleCache.
    > 
    > BUG=803276
    > TBR=dcheng@chromium.org
    > 
    > Change-Id: I4a7e868f2c65e2da67d362a97ab2d92fe1d0c120
    > Reviewed-on: https://chromium-review.googlesource.com/1166206
    > Reviewed-by: Mike Wittman <wittman@chromium.org>
    
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    BUG=803276
    
    Change-Id: Ieca1fc42a251087652a2cad0ea84a2a34cfc2cce
    Reviewed-on: https://chromium-review.googlesource.com/1169502
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: a944b0d7f322400a54def81b313f16b9e125167b

--- sampling_heap_profiler/module_cache.cc
+++ sampling_heap_profiler/module_cache.cc
@@ -5,27 +5,9 @@
 #include "base/sampling_heap_profiler/module_cache.h"
 
 #include "base/no_destructor.h"
+#include "base/profiler/native_stack_sampler.h"
 
 namespace base {
-
-ModuleCache::Module::Module() : is_valid(false) {}
-
-ModuleCache::Module::Module(uintptr_t base_address,
-                            const std::string& id,
-                            const FilePath& filename)
-    : Module(base_address, id, filename, 0) {}
-
-ModuleCache::Module::Module(uintptr_t base_address,
-                            const std::string& id,
-                            const FilePath& filename,
-                            size_t size)
-    : base_address(base_address),
-      id(id),
-      filename(filename),
-      is_valid(true),
-      size(size) {}
-
-ModuleCache::Module::~Module() = default;
 
 ModuleCache::ModuleCache() = default;
 ModuleCache::~ModuleCache() = default;
@@ -41,7 +23,7 @@ const ModuleCache::Module& ModuleCache::GetModuleForAddress(uintptr_t address) {
       return module;
   }
 
-  auto module = CreateModuleForAddress(address);
+  auto module = NativeStackSampler::GetModuleForAddress(address);
   if (!module.is_valid)
     return *invalid_module;
   return modules_cache_map_.emplace(module.base_address, std::move(module))

nwjs/chromium.src

commit 1c04255879f37c9baf3789a99dcf53de262b41da

Get TypeId to work with the msvc compiler.
Last attempt to land a unittests that makes sure that TypeId works
across shared libraries failed on MSVC and was reverted:
http://crrev/c/1375293

This patch keeps the current code for Clang. For MSVC instead of
using the address of a templated static variable as unique id it makes
use of the limited functionality the typeid operator provides when
compiling on MSVC without RTTI.

The only downside of using typeid is that it is not a constexpr and
thus many of the TypeId member methods can not be either. We could
remove constexpr-ness on MSVC builds only but to keep the interface
consistent across configurations this patch removes it for all of them.
We might want to revisit this decision later.

Bug: 914651

Change-Id: I458f41074fb04a6e459810fbc8c6d99a8d25707d
Reviewed-on: https://chromium-review.googlesource.com/c/1375952
Commit-Queue: Carlos Caballero <carlscab@google.com>
Reviewed-by: Alex Clarke <alexclarke@chromium.org>
Reviewed-by: Gabriel Charette <gab@chromium.org>
Cr-Commit-Position: refs/heads/master@{#628718}

base/type_id_unittest.cc
@@ -4,6 +4,8 @@
 
 #include "base/type_id.h"
 
+#include <memory>
+
 #include "base/test/type_id_test_support_a.h"
 #include "base/test/type_id_test_support_b.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -19,55 +21,39 @@ struct U {};
 }  // namespace
 
 TEST(TypeId, Basic) {
-  static_assert(TypeId::Create<int>() == TypeId::Create<int>(), "");
-  static_assert(TypeId::Create<int>() != TypeId::Create<void>(), "");
-  static_assert(TypeId::Create<int>() != TypeId::Create<float>(), "");
-  static_assert(TypeId::Create<int>() != TypeId::Create<std::unique_ptr<T>>(),
-                "");
-  static_assert(TypeId::Create<int>() != TypeId::Create<std::unique_ptr<U>>(),
-                "");
+  EXPECT_EQ(TypeId::From<int>(), TypeId::From<int>());
+  EXPECT_NE(TypeId::From<int>(), TypeId::From<void>());
+  EXPECT_NE(TypeId::From<int>(), TypeId::From<float>());
+  EXPECT_NE(TypeId::From<int>(), TypeId::From<std::unique_ptr<T>>());
+  EXPECT_NE(TypeId::From<int>(), TypeId::From<std::unique_ptr<U>>());
 
-  static_assert(TypeId::Create<void>() != TypeId::Create<int>(), "");
-  static_assert(TypeId::Create<void>() == TypeId::Create<void>(), "");
-  static_assert(TypeId::Create<void>() != TypeId::Create<float>(), "");
-  static_assert(TypeId::Create<void>() != TypeId::Create<std::unique_ptr<T>>(),
-                "");
-  static_assert(TypeId::Create<void>() != TypeId::Create<std::unique_ptr<U>>(),
-                "");
+  EXPECT_NE(TypeId::From<void>(), TypeId::From<int>());
+  EXPECT_EQ(TypeId::From<void>(), TypeId::From<void>());
+  EXPECT_NE(TypeId::From<void>(), TypeId::From<float>());
+  EXPECT_NE(TypeId::From<void>(), TypeId::From<std::unique_ptr<T>>());
+  EXPECT_NE(TypeId::From<void>(), TypeId::From<std::unique_ptr<U>>());
 
-  static_assert(TypeId::Create<float>() != TypeId::Create<int>(), "");
-  static_assert(TypeId::Create<float>() != TypeId::Create<void>(), "");
-  static_assert(TypeId::Create<float>() == TypeId::Create<float>(), "");
-  static_assert(TypeId::Create<float>() != TypeId::Create<std::unique_ptr<T>>(),
-                "");
-  static_assert(TypeId::Create<float>() != TypeId::Create<std::unique_ptr<U>>(),
-                "");
+  EXPECT_NE(TypeId::From<float>(), TypeId::From<int>());
+  EXPECT_NE(TypeId::From<float>(), TypeId::From<void>());
+  EXPECT_EQ(TypeId::From<float>(), TypeId::From<float>());
+  EXPECT_NE(TypeId::From<float>(), TypeId::From<std::unique_ptr<T>>());
+  EXPECT_NE(TypeId::From<float>(), TypeId::From<std::unique_ptr<U>>());
 
-  static_assert(TypeId::Create<std::unique_ptr<T>>() != TypeId::Create<int>(),
-                "");
-  static_assert(TypeId::Create<std::unique_ptr<T>>() != TypeId::Create<void>(),
-                "");
-  static_assert(TypeId::Create<std::unique_ptr<T>>() != TypeId::Create<float>(),
-                "");
-  static_assert(TypeId::Create<std::unique_ptr<T>>() ==
-                    TypeId::Create<std::unique_ptr<T>>(),
-                "");
-  static_assert(TypeId::Create<std::unique_ptr<T>>() !=
-                    TypeId::Create<std::unique_ptr<U>>(),
-                "");
+  EXPECT_NE(TypeId::From<std::unique_ptr<T>>(), TypeId::From<int>());
+  EXPECT_NE(TypeId::From<std::unique_ptr<T>>(), TypeId::From<void>());
+  EXPECT_NE(TypeId::From<std::unique_ptr<T>>(), TypeId::From<float>());
+  EXPECT_EQ(TypeId::From<std::unique_ptr<T>>(),
+            TypeId::From<std::unique_ptr<T>>());
+  EXPECT_NE(TypeId::From<std::unique_ptr<T>>(),
+            TypeId::From<std::unique_ptr<U>>());
 
-  static_assert(TypeId::Create<std::unique_ptr<U>>() != TypeId::Create<int>(),
-                "");
-  static_assert(TypeId::Create<std::unique_ptr<U>>() != TypeId::Create<void>(),
-                "");
-  static_assert(TypeId::Create<std::unique_ptr<U>>() != TypeId::Create<float>(),
-                "");
-  static_assert(TypeId::Create<std::unique_ptr<U>>() !=
-                    TypeId::Create<std::unique_ptr<T>>(),
-                "");
-  static_assert(TypeId::Create<std::unique_ptr<U>>() ==
-                    TypeId::Create<std::unique_ptr<U>>(),
-                "");
+  EXPECT_NE(TypeId::From<std::unique_ptr<U>>(), TypeId::From<int>());
+  EXPECT_NE(TypeId::From<std::unique_ptr<U>>(), TypeId::From<void>());
+  EXPECT_NE(TypeId::From<std::unique_ptr<U>>(), TypeId::From<float>());
+  EXPECT_NE(TypeId::From<std::unique_ptr<U>>(),
+            TypeId::From<std::unique_ptr<T>>());
+  EXPECT_EQ(TypeId::From<std::unique_ptr<U>>(),
+            TypeId::From<std::unique_ptr<U>>());
 }
 
 TEST(TypeId, TypesInAnonymousNameSpacesDontCollide) {
@@ -85,10 +71,10 @@ TEST(TypeId, IdenticalTypesFromDifferentCompilationUnitsMatch) {
             TypeIdTestSupportB::GetTypeIdForUniquePtrInt());
 }
 
-TEST(TypeId, DISABLED_IdenticalTypesFromComponentAndStaticLibrary) {
+TEST(TypeId, IdenticalTypesFromComponentAndStaticLibrary) {
   // Code generated for the test itself is statically linked. Make sure it works
   // with components
-  constexpr TypeId static_linked_type = TypeId::Create<std::unique_ptr<int>>();
+  TypeId static_linked_type = TypeId::From<std::unique_ptr<int>>();
   EXPECT_EQ(static_linked_type, TypeIdTestSupportA::GetTypeIdForUniquePtrInt());
 }
 

base/test/type_id_test_support_a.cc
@@ -17,11 +17,11 @@ namespace base {
 // static
 experimental::TypeId
 TypeIdTestSupportA::GetTypeIdForTypeInAnonymousNameSpace() {
-  return experimental::TypeId::Create<TypeInAnonymousNameSpace>();
+  return experimental::TypeId::From<TypeInAnonymousNameSpace>();
 }
 
 experimental::TypeId TypeIdTestSupportA::GetTypeIdForUniquePtrInt() {
-  return experimental::TypeId::Create<std::unique_ptr<int>>();
+  return experimental::TypeId::From<std::unique_ptr<int>>();
 }
 
 }  // namespace base

base/test/type_id_test_support_b.cc
@@ -17,11 +17,11 @@ namespace base {
 // static
 experimental::TypeId
 TypeIdTestSupportB::GetTypeIdForTypeInAnonymousNameSpace() {
-  return experimental::TypeId::Create<TypeInAnonymousNameSpace>();
+  return experimental::TypeId::From<TypeInAnonymousNameSpace>();
 }
 
 experimental::TypeId TypeIdTestSupportB::GetTypeIdForUniquePtrInt() {
-  return experimental::TypeId::Create<std::unique_ptr<int>>();
+  return experimental::TypeId::From<std::unique_ptr<int>>();
 }
 
 }  // namespace base

base/type_id.cc
@@ -9,11 +9,23 @@
 namespace base {
 namespace experimental {
 
+TypeId::TypeId(const char* function_name, internal::TypeUniqueId unique_type_id)
+    :
+#if DCHECK_IS_ON()
+      function_name_(function_name),
+#endif
+      unique_type_id_(unique_type_id) {
+}
+
+TypeId::TypeId() : TypeId("", internal::UniqueIdFromType<internal::NoType>()) {}
+
 std::string TypeId::ToString() const {
 #if DCHECK_IS_ON()
   return function_name_;
+#elif defined(COMPILER_MSVC)
+  return HexEncode(&unique_type_id_, sizeof(unique_type_id_));
 #else
-  return NumberToString(reinterpret_cast<uintptr_t>(type_id_));
+  return NumberToString(reinterpret_cast<uintptr_t>(unique_type_id_));
 #endif
 }
 

base/type_id.h
@@ -14,52 +14,85 @@
 #include "base/strings/string_piece.h"
 #include "build/build_config.h"
 
+#if defined(COMPILER_MSVC)
+#include <typeindex>
+#include <typeinfo>
+#endif
+
 namespace base {
+
 // Not ready for public consumption yet.
 namespace experimental {
+namespace internal {
 
+// The __attribute__((visibility("default"))) trick does not work for shared
+// libraries in MSVC. MSVC does support a reduced functionality typeid operator
+// with -fno-rtti, just enough for our needs.
+// Biggest drawback is that using typeid prevents us from having constexpr
+// methods (as the typeid operator is not constexpr)
+#if defined(COMPILER_MSVC)
+using TypeUniqueId = std::type_index;
+
+template <typename Type>
+inline TypeUniqueId UniqueIdFromType() {
+  return std::type_index(typeid(Type));
+}
+
+#else
 // A substitute for RTTI that uses the linker to uniquely reserve an address in
 // the binary for each type.
-class TypeId {
+// We need to make sure dummy_var has default visibility since we need to make
+// sure that there is only one definition across all libraries (shared or
+// static).
+template <typename Type>
+struct __attribute__((visibility("default"))) TypeTag {
+  static constexpr char dummy_var = 0;
+};
+
+// static
+template <typename Type>
+constexpr char TypeTag<Type>::dummy_var;
+
+using TypeUniqueId = const void*;
+
+template <typename Type>
+constexpr inline TypeUniqueId UniqueIdFromType() {
+  return &TypeTag<Type>::dummy_var;
+}
+#endif
+
+struct NoType {};
+
+}  // namespace internal
+
+class BASE_EXPORT TypeId {
  public:
-  bool constexpr operator==(const TypeId& other) const {
-    return type_id_ == other.type_id_;
+  template <typename T>
+  static TypeId From() {
+    return TypeId(PRETTY_FUNCTION, internal::UniqueIdFromType<T>());
   }
 
-  bool constexpr operator!=(const TypeId& other) const {
-    return !(*this == other);
+  TypeId();
+
+  TypeId(const TypeId& other) = default;
+  TypeId& operator=(const TypeId& other) = default;
+
+  bool operator==(TypeId other) const {
+    return unique_type_id_ == other.unique_type_id_;
   }
 
- public:
-  template <typename Type>
-  static constexpr TypeId Create() {
-    return TypeId(&TypeTag<Type>::dummy_var, PRETTY_FUNCTION);
-  }
+  bool operator!=(TypeId other) const { return !(*this == other); }
 
   std::string ToString() const;
 
  private:
-  template <typename Type>
-  struct TypeTag {
-    constexpr static char dummy_var = 0;
-  };
-
-  constexpr TypeId(const void* type_id, const char* function_name)
-      :
-#if DCHECK_IS_ON()
-        function_name_(function_name),
-#endif
-        type_id_(type_id) {
-  }
+  TypeId(const char* function_name, internal::TypeUniqueId unique_type_id);
 
 #if DCHECK_IS_ON()
-  const char* const function_name_;
+  const char* function_name_;
 #endif
-  const void* const type_id_;
+  internal::TypeUniqueId unique_type_id_;
 };
-
-template <typename Type>
-constexpr char TypeId::TypeTag<Type>::dummy_var;
 
 BASE_EXPORT std::ostream& operator<<(std::ostream& out, const TypeId& type_id);
 

yue/base

commit 8db6385e314daf4c008f581f746d0e6fe1b541df

Disable TypeId.IdenticalTypesFromComponentAndStaticLibrary on Android.
Started failing after 1c04255879f37c9baf3789a99dcf53de262b41da.

TBR: gab@chromium.org
Bug: 928806
Change-Id: I72d57344e0e6fca45aa9c944b3070fba07c19bb0
Reviewed-on: https://chromium-review.googlesource.com/c/1455041
Commit-Queue: Eric Stevenson <estevenson@chromium.org>
Reviewed-by: Eric Stevenson <estevenson@chromium.org>
Cr-Original-Commit-Position: refs/heads/master@{#629257}
Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
Cr-Mirrored-Commit: c206b4e047e6427181416e775780d587ee81717b

type_id_unittest.cc
@@ -8,6 +8,7 @@
 
 #include "base/test/type_id_test_support_a.h"
 #include "base/test/type_id_test_support_b.h"
+#include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace base {
@@ -70,7 +71,12 @@ TEST(TypeId, IdenticalTypesFromDifferentCompilationUnitsMatch) {
             TypeIdTestSupportB::GetTypeIdForUniquePtrInt());
 }
 
+// TODO(crbug.com/928806): Failing consistently on Android.
+#if defined(OS_ANDROID)
+TEST(TypeId, DISABLED_IdenticalTypesFromComponentAndStaticLibrary) {
+#else
 TEST(TypeId, IdenticalTypesFromComponentAndStaticLibrary) {
+#endif
   // Code generated for the test itself is statically linked. Make sure it works
   // with components
   TypeId static_linked_type = TypeId::From<std::unique_ptr<int>>();
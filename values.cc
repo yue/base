dbba42c7a6872b5994aad404bb75e288a132f847
Mathias Bynens <mathias@chromium.org>
2020-01-22 15:51:46 +0000

    Support Unicode non-characters in JSON{Parser,Writer}
    Per the JSON specification base::JSONParser aims to follow [1]
    (as well as JSON specifications that supersede it, such as [2]),
    JSON can encode Unicode non-characters. This patch adds support
    for such characters to base::JSONParser so that they can safely
    be transmitted via the Chrome DevTools Protocol. Thus, this patch
    removes the need for workarounds like [3] that were only needed
    due to lack of support.
    
    For symmetry, support for non-characters is added to
    base::JSONWriter as well.
    
    Note that although base::JSONParser and base::JSONWriter now
    support Unicode non-characters, invalid code points (i.e. lone
    surrogates) remain unsupported.
    
    Note that this change impacts JsonSanitizer as well: previously, if
    its input contains a non-character, JsonSanitizer would produce an
    error. Now, JsonSanitizer successfully generates sanitized JSON in
    this case, and as always, this sanitized JSON parses to the same
    exact value as the original JSON.
    
    Tests:
    
        autoninja -C out/Release content_shell base base_unittests services services_unittests
        out/Release/base_unittests --gtest_filter=JSONParserTest.*:JSONReaderTest.*:JSONStringEscapeTest.*:StringUtilTest.IsStringUTF8
        out/Release/services_unittests --gtest_filter=DataDecoderJsonSanitizerTest.Unicode
        third_party/blink/tools/run_web_tests.py -t Release http/tests/devtools/runtime/runtime-evaluate-bad-unicode.js
    
        autoninja -C out/Android services services_unittests
        out/Android/services_unittests --gtest_filter=DataDecoderJsonSanitizerTest.Unicode
    
    [1]: https://tools.ietf.org/html/rfc4627#section-2.5
    [2]: https://ecma-international.org/publications/standards/Ecma-404.htm
    [3]: https://chromium-review.googlesource.com/c/1352588
    
    Bug: chromium:848823, chromium:1042269
    Change-Id: Ib949425939517d85f1545da9d215dd2d137cedfa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2002813
    Commit-Queue: Mathias Bynens <mathias@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Auto-Submit: Mathias Bynens <mathias@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#734017}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: cdfff6b2120e54d1c770ae16d94944ca983afaf3

--- values.cc
+++ values.cc
@@ -220,7 +220,7 @@ Value::Value(StringPiece in_string) : Value(std::string(in_string)) {}
 
 Value::Value(std::string&& in_string) noexcept
     : type_(Type::STRING), string_value_(std::move(in_string)) {
-  DCHECK(IsStringUTF8(string_value_));
+  DCHECK(IsStringUTF8AllowingNoncharacters(string_value_));
 }
 
 Value::Value(const char16* in_string16) : Value(StringPiece16(in_string16)) {}
@@ -1204,7 +1204,7 @@ DictionaryValue::DictionaryValue(DictStorage&& in_dict) noexcept
     : Value(std::move(in_dict)) {}
 
 bool DictionaryValue::HasKey(StringPiece key) const {
-  DCHECK(IsStringUTF8(key));
+  DCHECK(IsStringUTF8AllowingNoncharacters(key));
   auto current_entry = dict_.find(key);
   DCHECK((current_entry == dict_.end()) || current_entry->second);
   return current_entry != dict_.end();
@@ -1215,7 +1215,7 @@ void DictionaryValue::Clear() {
 }
 
 Value* DictionaryValue::Set(StringPiece path, std::unique_ptr<Value> in_value) {
-  DCHECK(IsStringUTF8(path));
+  DCHECK(IsStringUTF8AllowingNoncharacters(path));
   DCHECK(in_value);
 
   // IMPORTANT NOTE: Do not replace with SetPathInternal() yet, because the
@@ -1290,7 +1290,7 @@ Value* DictionaryValue::SetWithoutPathExpansion(
 
 bool DictionaryValue::Get(StringPiece path,
                           const Value** out_value) const {
-  DCHECK(IsStringUTF8(path));
+  DCHECK(IsStringUTF8AllowingNoncharacters(path));
   const Value* value = FindPath(path);
   if (!value)
     return false;
@@ -1415,7 +1415,7 @@ bool DictionaryValue::GetList(StringPiece path, ListValue** out_value) {
 
 bool DictionaryValue::GetWithoutPathExpansion(StringPiece key,
                                               const Value** out_value) const {
-  DCHECK(IsStringUTF8(key));
+  DCHECK(IsStringUTF8AllowingNoncharacters(key));
   auto entry_iterator = dict_.find(key);
   if (entry_iterator == dict_.end())
     return false;
@@ -1520,7 +1520,7 @@ bool DictionaryValue::GetListWithoutPathExpansion(StringPiece key,
 
 bool DictionaryValue::Remove(StringPiece path,
                              std::unique_ptr<Value>* out_value) {
-  DCHECK(IsStringUTF8(path));
+  DCHECK(IsStringUTF8AllowingNoncharacters(path));
   StringPiece current_path(path);
   DictionaryValue* current_dictionary = this;
   size_t delimiter_position = current_path.rfind('.');
@@ -1538,7 +1538,7 @@ bool DictionaryValue::Remove(StringPiece path,
 bool DictionaryValue::RemoveWithoutPathExpansion(
     StringPiece key,
     std::unique_ptr<Value>* out_value) {
-  DCHECK(IsStringUTF8(key));
+  DCHECK(IsStringUTF8AllowingNoncharacters(key));
   auto entry_iterator = dict_.find(key);
   if (entry_iterator == dict_.end())
     return false;

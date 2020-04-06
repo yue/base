4a6bb12db05c583514b5030657a596f4a8637897
Hans Wennborg <hans@chromium.org>
2020-04-06 17:35:23 +0000

    Rewrite and expand unit tests for (PD)CHECK
    This is in preparation for refactoring the CHECK mechanism into its
    own header file, to reduce the complexity of logging.h and make these
    macros cheaper to use by putting them in a more light-weight header.
    
    The expanded tests are more comprehensive and include checking the
    exact log message etc.
    
    Bug: 1031540
    Change-Id: I4d8d134fbce684ccc0a74ef958854e0af9562d9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2135753
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#756716}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 78ea531d128c45a9bf5a3048f48d886c7d44ca60

--- logging_unittest.cc
+++ logging_unittest.cc
@@ -63,17 +63,70 @@ namespace {
 using ::testing::Return;
 using ::testing::_;
 
-// Needs to be global since log assert handlers can't maintain state.
-int g_log_sink_call_count = 0;
+// Helper class which expects a check to fire with a certain location and
+// message before the end of the current scope.
+class ScopedCheckExpectation {
+ public:
+  ScopedCheckExpectation(const char* file, int line, std::string msg)
+      : file_(file),
+        line_(line),
+        msg_(msg),
+        assert_handler_(base::BindRepeating(&ScopedCheckExpectation::Check,
+                                            base::Unretained(this))),
+        fired_(false) {}
+  ~ScopedCheckExpectation() {
+    EXPECT_TRUE(fired_) << "CHECK at " << file_ << ":" << line_
+                        << " never fired!";
+  }
 
-#if !defined(OFFICIAL_BUILD) || defined(DCHECK_ALWAYS_ON) || !defined(NDEBUG)
-void LogSink(const char* file,
+ private:
+  void Check(const char* file,
              int line,
-             const base::StringPiece message,
-             const base::StringPiece stack_trace) {
-  ++g_log_sink_call_count;
-}
+             const base::StringPiece msg,
+             const base::StringPiece stack) {
+    fired_ = true;
+    EXPECT_EQ(file, file_);
+    EXPECT_EQ(line, line_);
+    if (msg_.find("=~") == 0) {
+      EXPECT_THAT(std::string(msg), testing::MatchesRegex(msg_.substr(2)));
+    } else {
+      EXPECT_EQ(std::string(msg), msg_);
+    }
+  }
+
+  std::string file_;
+  int line_;
+  std::string msg_;
+  logging::ScopedLogAssertHandler assert_handler_;
+  bool fired_;
+};
+
+// Macro which expects a CHECK to fire with a certain message. If msg starts
+// with "=~", it's interpreted as a regular expression.
+// Example: EXPECT_CHECK("Check failed: false.", CHECK(false));
+#if defined(OFFICIAL_BUILD) && defined(NDEBUG)
+#define EXPECT_CHECK(msg, check_expr) \
+  do {                                \
+    EXPECT_DEATH(check_expr, "");     \
+  } while (0)
+#else
+#define EXPECT_CHECK(msg, check_expr)                          \
+  do {                                                         \
+    ScopedCheckExpectation check_exp(__FILE__, __LINE__, msg); \
+    check_expr;                                                \
+  } while (0)
 #endif
+
+// Macro which expects a DCHECK to fire if DCHECKs are enabled.
+#define EXPECT_DCHECK(msg, check_expr)                                 \
+  do {                                                                 \
+    if (DCHECK_IS_ON() && logging::LOG_DCHECK == logging::LOG_FATAL) { \
+      ScopedCheckExpectation check_exp(__FILE__, __LINE__, msg);       \
+      check_expr;                                                      \
+    } else {                                                           \
+      check_expr;                                                      \
+    }                                                                  \
+  } while (0)
 
 // Class to make sure any manipulations we do to the min log level are
 // contained (i.e., do not affect other unit tests).
@@ -83,7 +136,6 @@ class LogStateSaver {
 
   ~LogStateSaver() {
     SetMinLogLevel(old_min_log_level_);
-    g_log_sink_call_count = 0;
   }
 
  private:
@@ -375,32 +427,95 @@ TEST_F(LoggingTest, DuplicateLogFile) {
 }
 #endif  // defined(OS_CHROMEOS)
 
-// Official builds have CHECKs directly call BreakDebugger.
-#if !defined(OFFICIAL_BUILD)
+TEST_F(LoggingTest, CheckBasics) {
+  EXPECT_CHECK("Check failed: false. ", CHECK(false));
 
-// https://crbug.com/709067 tracks test flakiness on iOS.
-#if defined(OS_IOS)
-#define MAYBE_CheckStreamsAreLazy DISABLED_CheckStreamsAreLazy
-#else
-#define MAYBE_CheckStreamsAreLazy CheckStreamsAreLazy
-#endif
-TEST_F(LoggingTest, MAYBE_CheckStreamsAreLazy) {
-  MockLogSource mock_log_source, uncalled_mock_log_source;
-  EXPECT_CALL(mock_log_source, Log()).Times(8).
-      WillRepeatedly(Return("check message"));
-  EXPECT_CALL(uncalled_mock_log_source, Log()).Times(0);
+  EXPECT_CHECK("Check failed: false. foo", CHECK(false) << "foo");
 
-  ScopedLogAssertHandler scoped_assert_handler(base::BindRepeating(LogSink));
+  double a = 2, b = 1;
+  EXPECT_CHECK("Check failed: a < b (2 vs. 1)", CHECK_LT(a, b));
 
-  CHECK(mock_log_source.Log()) << uncalled_mock_log_source.Log();
-  PCHECK(!mock_log_source.Log()) << mock_log_source.Log();
-  CHECK_EQ(mock_log_source.Log(), mock_log_source.Log())
-      << uncalled_mock_log_source.Log();
-  CHECK_NE(mock_log_source.Log(), mock_log_source.Log())
-      << mock_log_source.Log();
+  EXPECT_CHECK("Check failed: a < b (2 vs. 1)foo", CHECK_LT(a, b) << "foo");
 }
 
+TEST_F(LoggingTest, PCheck) {
+  const char file[] = "/nonexistentfile123";
+  ignore_result(fopen(file, "r"));
+  std::string err =
+      logging::SystemErrorCodeToString(logging::GetLastSystemErrorCode());
+
+  EXPECT_CHECK(
+      "Check failed: fopen(file, \"r\") != nullptr."
+      " : " +
+          err,
+      PCHECK(fopen(file, "r") != nullptr));
+
+  EXPECT_CHECK(
+      "Check failed: fopen(file, \"r\") != nullptr."
+      " foo: " +
+          err,
+      PCHECK(fopen(file, "r") != nullptr) << "foo");
+
+  EXPECT_DCHECK(
+      "Check failed: fopen(file, \"r\") != nullptr."
+      " : " +
+          err,
+      DPCHECK(fopen(file, "r") != nullptr));
+
+  EXPECT_DCHECK(
+      "Check failed: fopen(file, \"r\") != nullptr."
+      " foo: " +
+          err,
+      DPCHECK(fopen(file, "r") != nullptr) << "foo");
+}
+
+TEST_F(LoggingTest, CheckOp) {
+  int a = 1, b = 2;
+  // clang-format off
+  EXPECT_CHECK("Check failed: a == b (1 vs. 2)", CHECK_EQ(a, b));
+  EXPECT_CHECK("Check failed: a != a (1 vs. 1)", CHECK_NE(a, a));
+  EXPECT_CHECK("Check failed: b <= a (2 vs. 1)", CHECK_LE(b, a));
+  EXPECT_CHECK("Check failed: b < a (2 vs. 1)",  CHECK_LT(b, a));
+  EXPECT_CHECK("Check failed: a >= b (1 vs. 2)", CHECK_GE(a, b));
+  EXPECT_CHECK("Check failed: a > b (1 vs. 2)",  CHECK_GT(a, b));
+
+  EXPECT_DCHECK("Check failed: a == b (1 vs. 2)", DCHECK_EQ(a, b));
+  EXPECT_DCHECK("Check failed: a != a (1 vs. 1)", DCHECK_NE(a, a));
+  EXPECT_DCHECK("Check failed: b <= a (2 vs. 1)", DCHECK_LE(b, a));
+  EXPECT_DCHECK("Check failed: b < a (2 vs. 1)",  DCHECK_LT(b, a));
+  EXPECT_DCHECK("Check failed: a >= b (1 vs. 2)", DCHECK_GE(a, b));
+  EXPECT_DCHECK("Check failed: a > b (1 vs. 2)",  DCHECK_GT(a, b));
+  // clang-format on
+}
+
+TEST_F(LoggingTest, CheckStreamsAreLazy) {
+  int called_count = 0;
+  int not_called_count = 0;
+
+  auto Called = [&]() {
+    ++called_count;
+    return 42;
+  };
+  auto NotCalled = [&]() {
+    ++not_called_count;
+    return 42;
+  };
+
+  CHECK(Called()) << NotCalled();
+  CHECK_EQ(Called(), Called()) << NotCalled();
+  PCHECK(Called()) << NotCalled();
+
+  DCHECK(Called()) << NotCalled();
+  DCHECK_EQ(Called(), Called()) << NotCalled();
+  DPCHECK(Called()) << NotCalled();
+
+  EXPECT_EQ(not_called_count, 0);
+#if DCHECK_IS_ON()
+  EXPECT_EQ(called_count, 8);
+#else
+  EXPECT_EQ(called_count, 4);
 #endif
+}
 
 #if defined(OFFICIAL_BUILD) && defined(OS_WIN)
 NOINLINE void CheckContainingFunc(int death_location) {
@@ -688,21 +803,6 @@ TEST_F(LoggingTest, DebugLoggingReleaseBehavior) {
   DVLOG_IF(1, debug_only_variable) << "test";
 }
 
-TEST_F(LoggingTest, DcheckStreamsAreLazy) {
-  MockLogSource mock_log_source;
-  EXPECT_CALL(mock_log_source, Log()).Times(0);
-#if DCHECK_IS_ON()
-  DCHECK(true) << mock_log_source.Log();
-  DCHECK_EQ(0, 0) << mock_log_source.Log();
-#else
-  DCHECK(mock_log_source.Log()) << mock_log_source.Log();
-  DPCHECK(mock_log_source.Log()) << mock_log_source.Log();
-  DCHECK_EQ(0, 0) << mock_log_source.Log();
-  DCHECK_EQ(mock_log_source.Log(), static_cast<const char*>(nullptr))
-      << mock_log_source.Log();
-#endif
-}
-
 void DcheckEmptyFunction1() {
   // Provide a body so that Release builds do not cause the compiler to
   // optimize DcheckEmptyFunction1 and DcheckEmptyFunction2 as a single
@@ -745,46 +845,35 @@ TEST_F(LoggingTest, MAYBE_Dcheck) {
   EXPECT_FALSE(DLOG_IS_ON(DCHECK));
 #elif defined(NDEBUG) && defined(DCHECK_ALWAYS_ON)
   // Release build with real DCHECKS.
-  ScopedLogAssertHandler scoped_assert_handler(base::BindRepeating(LogSink));
   EXPECT_TRUE(DCHECK_IS_ON());
   EXPECT_TRUE(DLOG_IS_ON(DCHECK));
 #else
   // Debug build.
-  ScopedLogAssertHandler scoped_assert_handler(base::BindRepeating(LogSink));
   EXPECT_TRUE(DCHECK_IS_ON());
   EXPECT_TRUE(DLOG_IS_ON(DCHECK));
 #endif
 
-  // DCHECKs are fatal iff they're compiled in DCHECK_IS_ON() and the DCHECK
-  // log level is set to fatal.
-  const bool dchecks_are_fatal = DCHECK_IS_ON() && LOG_DCHECK == LOG_FATAL;
-  EXPECT_EQ(0, g_log_sink_call_count);
-  DCHECK(false);
-  EXPECT_EQ(dchecks_are_fatal ? 1 : 0, g_log_sink_call_count);
-  DPCHECK(false);
-  EXPECT_EQ(dchecks_are_fatal ? 2 : 0, g_log_sink_call_count);
-  DCHECK_EQ(0, 1);
-  EXPECT_EQ(dchecks_are_fatal ? 3 : 0, g_log_sink_call_count);
+  EXPECT_DCHECK("Check failed: false. ", DCHECK(false));
+  std::string err =
+      logging::SystemErrorCodeToString(logging::GetLastSystemErrorCode());
+  EXPECT_DCHECK("Check failed: false. : " + err, DPCHECK(false));
+  EXPECT_DCHECK("Check failed: 0 == 1 (0 vs. 1)", DCHECK_EQ(0, 1));
 
   // Test DCHECK on std::nullptr_t
-  g_log_sink_call_count = 0;
   const void* p_null = nullptr;
   const void* p_not_null = &p_null;
   DCHECK_EQ(p_null, nullptr);
   DCHECK_EQ(nullptr, p_null);
   DCHECK_NE(p_not_null, nullptr);
   DCHECK_NE(nullptr, p_not_null);
-  EXPECT_EQ(0, g_log_sink_call_count);
 
   // Test DCHECK on a scoped enum.
   enum class Animal { DOG, CAT };
   DCHECK_EQ(Animal::DOG, Animal::DOG);
-  EXPECT_EQ(0, g_log_sink_call_count);
-  DCHECK_EQ(Animal::DOG, Animal::CAT);
-  EXPECT_EQ(dchecks_are_fatal ? 1 : 0, g_log_sink_call_count);
+  EXPECT_DCHECK("Check failed: Animal::DOG == Animal::CAT (0 vs. 1)",
+                DCHECK_EQ(Animal::DOG, Animal::CAT));
 
   // Test DCHECK on functions and function pointers.
-  g_log_sink_call_count = 0;
   struct MemberFunctions {
     void MemberFunction1() {
       // See the comment in DcheckEmptyFunction1().
@@ -798,48 +887,50 @@ TEST_F(LoggingTest, MAYBE_Dcheck) {
   void (*fp2)() = DcheckEmptyFunction2;
   void (*fp3)() = DcheckEmptyFunction1;
   DCHECK_EQ(fp1, fp3);
-  EXPECT_EQ(0, g_log_sink_call_count);
   DCHECK_EQ(mp1, &MemberFunctions::MemberFunction1);
-  EXPECT_EQ(0, g_log_sink_call_count);
   DCHECK_EQ(mp2, &MemberFunctions::MemberFunction2);
-  EXPECT_EQ(0, g_log_sink_call_count);
-  DCHECK_EQ(fp1, fp2);
-  EXPECT_EQ(dchecks_are_fatal ? 1 : 0, g_log_sink_call_count);
-  DCHECK_EQ(mp2, &MemberFunctions::MemberFunction1);
-  EXPECT_EQ(dchecks_are_fatal ? 2 : 0, g_log_sink_call_count);
+  EXPECT_DCHECK("=~Check failed: fp1 == fp2 \\(\\w+ vs. \\w+\\)",
+                DCHECK_EQ(fp1, fp2));
+  EXPECT_DCHECK(
+      "Check failed: mp2 == &MemberFunctions::MemberFunction1 (1 vs. 1)",
+      DCHECK_EQ(mp2, &MemberFunctions::MemberFunction1));
 }
 
 TEST_F(LoggingTest, DcheckReleaseBehavior) {
-  int some_variable = 1;
-  // These should still reference |some_variable| so we don't get
-  // unused variable warnings.
-  DCHECK(some_variable) << "test";
-  DPCHECK(some_variable) << "test";
-  DCHECK_EQ(some_variable, 1) << "test";
+  int var1 = 1;
+  int var2 = 2;
+  int var3 = 3;
+  int var4 = 4;
+
+  // No warnings about unused variables even though no check fires and DCHECK
+  // may or may not be enabled.
+  DCHECK(var1) << var2;
+  DPCHECK(var1) << var3;
+  DCHECK_EQ(var1, 1) << var4;
 }
 
 TEST_F(LoggingTest, DCheckEqStatements) {
   bool reached = false;
   if (false)
-    DCHECK_EQ(false, true);           // Unreached.
+    DCHECK_EQ(false, true);  // Unreached.
   else
     DCHECK_EQ(true, reached = true);  // Reached, passed.
   ASSERT_EQ(DCHECK_IS_ON() ? true : false, reached);
 
   if (false)
-    DCHECK_EQ(false, true);           // Unreached.
+    DCHECK_EQ(false, true);  // Unreached.
 }
 
 TEST_F(LoggingTest, CheckEqStatements) {
   bool reached = false;
   if (false)
-    CHECK_EQ(false, true);           // Unreached.
+    CHECK_EQ(false, true);  // Unreached.
   else
     CHECK_EQ(true, reached = true);  // Reached, passed.
   ASSERT_TRUE(reached);
 
   if (false)
-    CHECK_EQ(false, true);           // Unreached.
+    CHECK_EQ(false, true);  // Unreached.
 }
 
 TEST_F(LoggingTest, NestedLogAssertHandlers) {
@@ -913,15 +1004,8 @@ TEST_F(LoggingTest, ConfigurableDCheck) {
 
   // Verify that DCHECK does crash if LOG_DCHECK is set to LOG_FATAL.
   LOG_DCHECK = LOG_FATAL;
-
-  ::testing::StrictMock<MockLogAssertHandler> handler;
-  EXPECT_CALL(handler, HandleLogAssert(_, _, _, _)).Times(2);
-  {
-    logging::ScopedLogAssertHandler scoped_handler_b(base::BindRepeating(
-        &MockLogAssertHandler::HandleLogAssert, base::Unretained(&handler)));
-    DCHECK(false);
-    DCHECK_EQ(1, 2);
-  }
+  EXPECT_CHECK("Check failed: false. ", DCHECK(false));
+  EXPECT_CHECK("Check failed: 1 == 2 (1 vs. 2)", DCHECK_EQ(1, 2));
 }
 
 TEST_F(LoggingTest, ConfigurableDCheckFeature) {
@@ -1125,44 +1209,42 @@ TEST_F(LoggingTest, LogMessageMarkersOnStack) {
 }
 #endif  // !defined(ADDRESS_SANITIZER)
 
-const char* kToStringResult = "to_string";
-const char* kOstreamResult = "ostream";
-
-struct StructWithOstream {};
-
+struct StructWithOstream {
+  bool operator==(const StructWithOstream& o) const { return &o == this; }
+};
+#if !(defined(OFFICIAL_BUILD) && defined(NDEBUG))
 std::ostream& operator<<(std::ostream& out, const StructWithOstream&) {
-  return out << kOstreamResult;
+  return out << "ostream";
 }
-
-TEST(MakeCheckOpValueStringTest, HasOnlyOstream) {
-  std::ostringstream oss;
-  logging::MakeCheckOpValueString(&oss, StructWithOstream());
-  EXPECT_EQ(kOstreamResult, oss.str());
-}
+#endif
 
 struct StructWithToString {
-  std::string ToString() const { return kToStringResult; }
+  bool operator==(const StructWithToString& o) const { return &o == this; }
+  std::string ToString() const { return "ToString"; }
 };
-
-TEST(MakeCheckOpValueStringTest, HasOnlyToString) {
-  std::ostringstream oss;
-  logging::MakeCheckOpValueString(&oss, StructWithToString());
-  EXPECT_EQ(kToStringResult, oss.str());
-}
 
 struct StructWithToStringAndOstream {
-  std::string ToString() const { return kToStringResult; }
+  bool operator==(const StructWithToStringAndOstream& o) const {
+    return &o == this;
+  }
+  std::string ToString() const { return "ToString"; }
 };
-
+#if !(defined(OFFICIAL_BUILD) && defined(NDEBUG))
 std::ostream& operator<<(std::ostream& out,
                          const StructWithToStringAndOstream&) {
-  return out << kOstreamResult;
+  return out << "ostream";
 }
+#endif
 
-TEST(MakeCheckOpValueStringTest, HasOstreamAndToString) {
-  std::ostringstream oss;
-  logging::MakeCheckOpValueString(&oss, StructWithToStringAndOstream());
-  EXPECT_EQ(kOstreamResult, oss.str());
+TEST_F(LoggingTest, OstreamVsToString) {
+  StructWithOstream a, b;
+  EXPECT_CHECK("Check failed: a == b (ostream vs. ostream)", CHECK_EQ(a, b));
+
+  StructWithToString c, d;
+  EXPECT_CHECK("Check failed: c == d (ToString vs. ToString)", CHECK_EQ(c, d));
+
+  StructWithToStringAndOstream e, f;
+  EXPECT_CHECK("Check failed: e == f (ostream vs. ostream)", CHECK_EQ(e, f));
 }
 
 }  // namespace

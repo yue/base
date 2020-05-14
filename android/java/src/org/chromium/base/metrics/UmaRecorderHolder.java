ece99fb201cd2a1317a026858239acedfa1bb1b2
Hazem Ashmawy <hazems@google.com>
2020-05-14 22:52:51 +0000

    Implement WebView UmaRecorder for non-embedded processes
    Make AwNonembeddedUmaRecorder an implementation of the
    base.metrics.UmaRecorder interface and set it as a delegate in the
    CacheUmaRecorder during the startup of WebView non-embedded process.
    This will make using RecordHistogram class possible inside WebView
    non-embedded processes.
    
    Bug: 1001185
    Test: out/Debug/bin/run_webview_instrumentation_test_apk
    Test: out/Debug/bin/run_android_webview_junit_tests
    Change-Id: Ia324f30f45a334a61bf46a1bf64d69715ad7118a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2176771
    Commit-Queue: Hazem Ashmawy <hazems@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#769021}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 1a533cf20ed7f814532040ba17b16e422c4eb586

--- android/java/src/org/chromium/base/metrics/UmaRecorderHolder.java
+++ android/java/src/org/chromium/base/metrics/UmaRecorderHolder.java
@@ -13,13 +13,44 @@ public class UmaRecorderHolder {
     /** The instance held by this class. */
     private static CachingUmaRecorder sRecorder = new CachingUmaRecorder();
 
-    /** Returns the {@link CachingUmaRecorder}. */
-    /* package */ static CachingUmaRecorder get() {
+    /** Allow calling onLibraryLoaded() */
+    private static boolean sAllowNativeUmaRecorder = true;
+
+    /** Returns the held {@link UmaRecorder}. */
+    public static UmaRecorder get() {
         return sRecorder;
     }
 
-    /** Starts forwarding metrics to the native code. Returns after the cache has been flushed. */
+    /**
+     * Set a new {@link UmaRecorder} delegate for the {@link CachingUmaRecorder}.
+     * Returns after the cache has been flushed to the new delegate.
+     * <p>
+     * It should be used in processes that don't or haven't loaded native yet. This should never
+     * be called after calling {@link #onLibraryLoaded()}.
+     *
+     * @param recorder the new UmaRecorder that metrics will be forwarded to.
+     */
+    public static void setNonNativeDelegate(UmaRecorder recorder) {
+        UmaRecorder previous = sRecorder.setDelegate(recorder);
+        assert !(previous instanceof NativeUmaRecorder)
+            : "A NativeUmaRecorder has already been set";
+    }
+
+    /**
+     * Disallow calling onLibraryLoaded() to set a {@link NativeUmaRecorder}.
+     * <p>
+     * Can be used in processes that don't load native library to make sure that a native recorder
+     * is never set to avoid possible breakage.
+     */
+    public static void setAllowNativeUmaRecorder(boolean allow) {
+        sAllowNativeUmaRecorder = allow;
+    }
+
+    /**
+     * Starts forwarding metrics to the native code. Returns after the cache has been flushed.
+     */
     public static void onLibraryLoaded() {
+        assert sAllowNativeUmaRecorder : "Setting NativeUmaRecorder is not allowed";
         sRecorder.setDelegate(new NativeUmaRecorder());
     }
 

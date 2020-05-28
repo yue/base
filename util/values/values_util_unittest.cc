28ec26b55392090813ff4042392ef25a51b56ce8
Jan Wilken Dörrie <jdoerrie@chromium.org>
2020-05-28 19:48:17 +0000

    [util] Minor util/values clean-ups
    This change addresses comments left on the review of r772634.
    
    Bug: 1086733
    Change-Id: I85020ddc489eb91b166026b723d9b9333c81116a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2219949
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Auto-Submit: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#772836}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: e4d31a6dffbbf70caf137b6d4abd0745c33ce520

--- util/values/values_util_unittest.cc
+++ util/values/values_util_unittest.cc
@@ -2,11 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <limits>
-
 #include "base/util/values/values_util.h"
 
+#include <limits>
+
 #include "base/files/file_path.h"
+#include "base/strings/string_piece.h"
 #include "base/unguessable_token.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -15,17 +16,17 @@ namespace util {
 namespace {
 
 TEST(ValuesUtilTest, BasicInt64Limits) {
-  struct {
+  constexpr struct {
     int64_t input;
-    const char* expected;
-  } test_cases[] = {
+    base::StringPiece expected;
+  } kTestCases[] = {
       {0, "0"},
       {-1234, "-1234"},
       {5678, "5678"},
       {std::numeric_limits<int64_t>::lowest(), "-9223372036854775808"},
       {std::numeric_limits<int64_t>::max(), "9223372036854775807"},
   };
-  for (const auto& test_case : test_cases) {
+  for (const auto& test_case : kTestCases) {
     int64_t input = test_case.input;
     base::TimeDelta time_delta_input = base::TimeDelta::FromMicroseconds(input);
     base::Time time_input =
@@ -46,7 +47,7 @@ TEST(ValuesUtilTest, BasicInt64Limits) {
 }
 
 TEST(ValuesUtilTest, InvalidInt64Values) {
-  std::unique_ptr<base::Value> test_cases[] = {
+  const std::unique_ptr<base::Value> kTestCases[] = {
       nullptr,
       std::make_unique<base::Value>(),
       std::make_unique<base::Value>(0),
@@ -61,7 +62,7 @@ TEST(ValuesUtilTest, InvalidInt64Values) {
       std::make_unique<base::Value>("1234a"),
       std::make_unique<base::Value>("a1234"),
   };
-  for (const auto& test_case : test_cases) {
+  for (const auto& test_case : kTestCases) {
     EXPECT_FALSE(ValueToInt64(test_case.get()));
     EXPECT_FALSE(ValueToTimeDelta(test_case.get()));
     EXPECT_FALSE(ValueToTime(test_case.get()));
@@ -70,11 +71,11 @@ TEST(ValuesUtilTest, InvalidInt64Values) {
 
 TEST(ValuesUtilTest, FilePath) {
   // Ω is U+03A9 GREEK CAPITAL LETTER OMEGA, a non-ASCII character.
-  std::string test_cases[] = {
+  constexpr base::StringPiece kTestCases[] = {
       "/unix/Ω/path.dat",
       "C:\\windows\\Ω\\path.dat",
   };
-  for (const auto& test_case : test_cases) {
+  for (auto test_case : kTestCases) {
     base::FilePath input = base::FilePath::FromUTF8Unsafe(test_case);
     base::Value expected(test_case);
     SCOPED_TRACE(testing::Message() << "test_case: " << test_case);
@@ -85,14 +86,14 @@ TEST(ValuesUtilTest, FilePath) {
 }
 
 TEST(ValuesUtilTest, UnguessableToken) {
-  struct {
+  constexpr struct {
     uint64_t high;
     uint64_t low;
-    const char* expected;
-  } test_cases[] = {
+    base::StringPiece expected;
+  } kTestCases[] = {
       {0x123456u, 0x9ABCu, "5634120000000000BC9A000000000000"},
   };
-  for (const auto& test_case : test_cases) {
+  for (const auto& test_case : kTestCases) {
     base::UnguessableToken input =
         base::UnguessableToken::Deserialize(test_case.high, test_case.low);
     base::Value expected(test_case.expected);

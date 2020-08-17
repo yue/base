666c9adea8285ed7fd42d83a5c66b518c814a4b6
Malay Keshav <malaykeshav@chromium.org>
2020-08-17 17:50:19 +0000

    Viz: Add composite time trace events to measure draw time per quad
    This patch adds trace events using GPU timer queries to measure the
    composite time per frame. The trace events are per quads and uses the
    ready timestamp to synchronize with the trace timeline.
    
    NOTE: This only works on intel mesa based devices. Other drivers may
    not work as expected.
    
    Bug: 1092700
    Test: Manually ran trace events on atlas device.
    Change-Id: I1c8ee41e2c769129e28a9a3d28bfe77dd1ddd8fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2284010
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Commit-Queue: Malay Keshav <malaykeshav@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798701}
    GitOrigin-RevId: a78161bf7ee619ca64e2188a19248f482e5426ae

--- trace_event/builtin_categories.h
+++ trace_event/builtin_categories.h
@@ -242,6 +242,7 @@
   X(TRACE_DISABLED_BY_DEFAULT("v8.turbofan"))                            \
   X(TRACE_DISABLED_BY_DEFAULT("v8.wasm.detailed"))                       \
   X(TRACE_DISABLED_BY_DEFAULT("video_and_image_capture"))                \
+  X(TRACE_DISABLED_BY_DEFAULT("viz.gpu_composite_time"))                 \
   X(TRACE_DISABLED_BY_DEFAULT("viz.debug.overlay_planes"))               \
   X(TRACE_DISABLED_BY_DEFAULT("viz.hit_testing_flow"))                   \
   X(TRACE_DISABLED_BY_DEFAULT("viz.overdraw"))                           \

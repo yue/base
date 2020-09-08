b69552584af6a1eb94a59b1f8c3a59861de09de4
Cheng Zhao <zcbenz@gmail.com>
2020-09-08 09:46:36 +0900

    Fix compilation with non-clang

--- check.cc
+++ check.cc
@@ -8,7 +8,6 @@
 // build time. Try not to raise this limit unless absolutely necessary. See
 // https://chromium.googlesource.com/chromium/src/+/HEAD/docs/wmax_tokens.md
 #ifndef NACL_TC_REV
-#pragma clang max_tokens_here 17000
 #endif
 
 #include "base/check_op.h"
